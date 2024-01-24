8680b830124209dde67dd7d36e150959d2511ad1
Joel Varughese <jtvdev126@gmail.com>
2024-01-24 17:04:05 -0600

    delete_note complete

--- delete_note_lambda.py
+++ delete_note_lambda.py
@@ -6,3 +6,16 @@ import boto3
 dynamodb = boto3.resource('dynamodb')
 table = dynamodb.Table('Notes')
 
+def lambda_handler(event,context):
+    note_id = event['pathParameters']['note_id']
+
+    table.delete_item(
+        Key={
+            'note_id': note_id
+        }
+    )
+
+    return {
+        'statusCode': 200,
+        'body': json.dumps('Note deleted successfully.')
+    }
