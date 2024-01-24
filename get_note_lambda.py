cc5ccdc282622b7508635187a60665cebdf4c480
Joel Varughese <jtvdev126@gmail.com>
2024-01-24 16:48:53 -0600

    added imports, tables, handler to get_note

--- get_note_lambda.py
+++ get_note_lambda.py
@@ -1,2 +1,23 @@
 # Create Lambda Function for Read Operation (GetNote)
 
+import json
+import boto3
+
+dynamodb = boto3.resource('dynamodb')
+table = dynamodb.Table('Notes')
+
+def lambda_handler(event, context):
+    note_id = event['pathParameters']['note_id']
+
+    response = table.get_item(
+        Key={
+            'note_id': note_id
+        }
+    )
+    
+    item = response.get('Item', {})
+
+    return {
+        'statusCode': 200,
+        'body': json.dumps(item)
+    }
