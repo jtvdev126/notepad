8ef23cea1c2193596368eed1a54b29ca555cbf82
Joel Varughese <jtvdev126@gmail.com>
2024-01-24 16:53:46 -0600

    imported json and boto

--- update_note_lambda.py
+++ update_note_lambda.py
@@ -1,1 +1,10 @@
 # Create Lambda Function for Update Operation (UpdateNote)
+
+import json
+import boto3
+
+dynamodb = boto3.resource('dynamodb')
+table = dynamodb.Table('Notes')
+
+def lambda_handler(event,context):
+    
