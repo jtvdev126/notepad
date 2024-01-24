ebba6f58d6f75e33949dc8ec0748db9c3aac989d
Joel Varughese <jtvdev126@gmail.com>
2024-01-24 16:57:37 -0600

    table for update item

--- update_note_lambda.py
+++ update_note_lambda.py
@@ -8,4 +8,14 @@ table = dynamodb.Table('Notes')
 
 def lambda_handler(event,context):
     data = json.loads(event['body'])
-    
+    note_id = event['pathParameters']['note_id']
+
+    table.update_item(
+        Key={
+            'note_id': note_id
+        },
+        UpdateExpression='SET content = :content',
+        ExpressionAttributeValues={
+            ':content':data['content']
+        }
+    )
